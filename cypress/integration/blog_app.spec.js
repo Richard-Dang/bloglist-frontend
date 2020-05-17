aa277912f712e924563c6f90d3c72444d9e153a5
Richard Dang <richardspacedang@gmail.com>
2020-05-17 01:32:54 -0400

    Finish 5.21 - 5.22

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -33,10 +33,11 @@ describe("Blog app", function () {
     });
   });
 
-  describe.only("when logged in", function () {
-    this.beforeEach(function () {
+  describe("when logged in", function () {
+    beforeEach(function () {
       cy.login({ username: "richarddang", password: "password" });
     });
+
     it("a blog can be created", function () {
       cy.contains("new blog").click();
       cy.get("#title").type("Title of Blog");
@@ -45,6 +46,61 @@ describe("Blog app", function () {
       cy.get("#create-button").click();
       cy.contains("a new blog Title of Blog by Richard Dang added");
       cy.contains("Title of Blog Richard Dang");
+    });
+
+    describe("and blogs exists", function () {
+      beforeEach(function () {
+        cy.createBlog({
+          author: "Richard Dang",
+          title: "Title of Blog",
+          url: "http://blog.com",
+        });
+        cy.createBlog({
+          author: "Brad Pitt",
+          title: "Title of Blog 2",
+          url: "http://blog2.com",
+          likes: 10,
+        });
+        cy.createBlog({
+          author: "Samuel Jackson",
+          title: "Title of Blog",
+          url: "http://blog3.com",
+          likes: 20,
+        });
+      });
+      it.only("blogs are ordered in ascending order according to likes", function () {
+        cy.get(".blog").then((blogs) => {
+          let prev = Number.MAX_SAFE_INTEGER;
+          for (var i = 0; i < blogs.length; i++) {
+            cy.wrap(blogs[i]).contains("view").click();
+            cy.wrap(blogs[i])
+              .find(".likes")
+              .then((e) => {
+                const likes = parseInt(e.text());
+                expect(prev).to.be.greaterThan(likes);
+                prev = likes;
+              });
+          }
+        });
+      });
+
+      it("a blog can be liked", function () {
+        cy.contains("Title of Blog Richard Dang")
+          .parent()
+          .find("button")
+          .click();
+        cy.contains("like").click();
+        cy.contains("like").click();
+        cy.contains("likes 2");
+      });
+      it("a blog can be deleted by the user who created it", function () {
+        cy.contains("Title of Blog Richard Dang")
+          .parent()
+          .find("button")
+          .click();
+        cy.contains("remove").click();
+        cy.get("html").should("not.contain", "Title of Blog Richard Dang");
+      });
     });
   });
 });
